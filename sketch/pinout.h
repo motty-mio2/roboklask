40b6675d1fe9000f8a7f4f709f6a1c6b2728419c
motty <motty.mio2@gmail.com>
2026-06-02 22:11:25 +0900

    add pinout

---
+++ sketch/pinout.h
@@ -0,0 +1,14 @@
+constexpr uint8_t M0 = 0;
+constexpr uint8_t M1 = 1;
+constexpr uint8_t M2 = 2;
+constexpr uint8_t nEN = 3;
+constexpr uint8_t MY_STEP = 4;
+constexpr uint8_t MY_DIR = 5;
+constexpr uint8_t MX_STEP = 8;
+constexpr uint8_t MY_DIR = 9;
+constexpr uint8_t VEN = 10;
+
+constexpr uint8_t SW_A = 12;
+constexpr uint8_t SW_B = 13;
+constexpr uint8_t SW_X = 20;
+constexpr uint8_t SW_Y = 21;
