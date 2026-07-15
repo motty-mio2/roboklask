7603134aa50ed266327f4788ad2a2c473d54dd33
motty <motty.mio2@gmail.com>
2026-07-15 23:20:08 +0900

    Revert "ほんと？"
    This reverts commit 95edebf1c3d6137e1974746877e7e888a4c1a7fc.

--- sketch/matrix.h
+++ sketch/matrix.h
@@ -20,9 +20,8 @@ const uint8_t* generate_matrix(const float& x, const float& y) {
   static uint8_t led[104];
   memset(led, 0, sizeof(led));
 
-  // xx（横方向）に x（左右座標）、yy（縦方向）に y（前後座標）をマッピングする
-  int xx = int(round(((std::min(std::max(x, -1.0f), 1.0f) + 1.0f) / 2.0f) * (WIDTH - 1)));
-  int yy = int(round(((std::min(std::max(y, -1.0f), 1.0f) + 1.0f) / 2.0f) * (HEIGHT - 1)));
+  int xx = int(std::min(std::max(y, -1.0f), 1.0f) * HALF_WIDTH + HALF_WIDTH);
+  int yy = int(std::min(std::max(x, 0.0f), 1.0f) * (HEIGHT - 1));
 
   led[xx + yy * WIDTH] = 1;
 
