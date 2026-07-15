9556cde47f30d27b8b72056629e4f32d13447fc6
motty <motty.mio2@gmail.com>
2026-07-15 23:25:43 +0900

    format

--- sketch/matrix.h
+++ sketch/matrix.h
@@ -20,7 +20,7 @@ const uint8_t* generate_matrix(const float& x, const float& y) {
   static uint8_t led[104];
   memset(led, 0, sizeof(led));
 
-  int xx = int((std::min(std::max(y, -1.0f), 1.0f) + 1.0f) * WIDTH / 2);
+  int xx = int((std::min(std::max(y, -1.0f), 1.0f) + 1.0f) / 2 * (WIDTH - 1));
   int yy = int(std::min(std::max(x, 0.0f), 1.0f) * (HEIGHT - 1));
 
   led[xx + yy * WIDTH] = 1;
