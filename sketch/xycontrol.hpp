91ac3a163eab1191ec7c80a91fc0077647a35fd4
motty <motty.mio2@gmail.com>
2026-08-19 22:46:02 +0900

    tuning: adjust speed and acceleration to safer limits to prevent step loss

--- sketch/xycontrol.hpp
+++ sketch/xycontrol.hpp
@@ -21,17 +21,17 @@ private:
 
 public:
   void setHomingSpeed() {
-    stepper1.setMaxSpeed(STEP * 7 * resolution);
-    stepper1.setAcceleration(STEP * 7 * resolution);
-    stepper2.setMaxSpeed(STEP * 7 * resolution);
-    stepper2.setAcceleration(STEP * 7 * resolution);
+    stepper1.setMaxSpeed(STEP * 5 * resolution);
+    stepper1.setAcceleration(STEP * 5 * resolution);
+    stepper2.setMaxSpeed(STEP * 5 * resolution);
+    stepper2.setAcceleration(STEP * 5 * resolution);
   }
 
   void setOperationalSpeed() {
-    stepper1.setMaxSpeed(STEP * 15 * resolution);
-    stepper2.setMaxSpeed(STEP * 15 * resolution);
-    stepper1.setAcceleration(STEP * 8 * resolution);
-    stepper2.setAcceleration(STEP * 8 * resolution);
+    stepper1.setMaxSpeed(STEP * 10 * resolution);
+    stepper2.setMaxSpeed(STEP * 10 * resolution);
+    stepper1.setAcceleration(STEP * 4 * resolution);
+    stepper2.setAcceleration(STEP * 4 * resolution);
   }
 
   XYControl(const int m1_step, const int m1_dir, const int m2_step,
