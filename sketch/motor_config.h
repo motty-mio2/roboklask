1b7d2386e8cbb5fca172f7f52bbff6fabdc04d81
motty <motty.mio2@gmail.com>
2026-08-20 22:09:33 +0900

    tuning: set resolution to 8 (1/8 step) to double the travel distance, and increase STEP_BACK to 300

--- sketch/motor_config.h
+++ sketch/motor_config.h
@@ -1,16 +1,16 @@
 #ifndef MOTOR_CONFIG_H
 #define MOTOR_CONFIG_H_
 
-constexpr int setM0 = false;
+constexpr int setM0 = true;
 constexpr int setM1 = true;
 constexpr int setM2 = false;
 
 // 1回転あたりのベースステップ数
 constexpr int STEP = 400;
-constexpr int resolution = 4;
+constexpr int resolution = 8;
 
 // 盤面の可動最大ステップ数
-constexpr int STEP_BACK = 100;
+constexpr int STEP_BACK = 300;
 constexpr int XSTEP = 3100 - STEP_BACK * 2;
 constexpr int YSTEP = 1500 - STEP_BACK;
 
