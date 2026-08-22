d6917f3560cf4bc25802efbeaeac54170bd2999c
motty <motty.mio2@gmail.com>
2026-08-22 19:01:53 +0900

    tuning: restore resolution = 8 (1/8 step) to maintain coordinate accuracy, and set operational speed to STEP * 3.5

--- sketch/motor_config.h
+++ sketch/motor_config.h
@@ -1,13 +1,13 @@
 #ifndef MOTOR_CONFIG_H
 #define MOTOR_CONFIG_H_
 
-constexpr int setM0 = false;
+constexpr int setM0 = true;
 constexpr int setM1 = true;
 constexpr int setM2 = false;
 
 // 1回転あたりのベースステップ数
 constexpr int STEP = 400;
-constexpr int resolution = 4;
+constexpr int resolution = 8;
 
 // 盤面の可動最大ステップ数
 constexpr int STEP_BACK = 100;     // ソフトウェアの可動範囲マージン
