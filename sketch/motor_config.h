77a4c3aa07ce90ab4156e1286d7b1123c1ff8e36
motty <motty.mio2@gmail.com>
2026-08-22 21:24:58 +0900

    more step back

--- sketch/motor_config.h
+++ sketch/motor_config.h
@@ -11,7 +11,7 @@ constexpr int resolution = 8;
 
 // 盤面の可動最大ステップ数
 constexpr int STEP_BACK_X = 100;   // X軸のソフトウェア可動範囲マージン
-constexpr int STEP_BACK_Y = 100;   // Y軸のソフトウェア可動範囲マージン
+constexpr int STEP_BACK_Y = 200;   // Y軸のソフトウェア可動範囲マージン
 constexpr int PHYSICAL_BACK = 300; // スイッチ解除のための物理的な戻り量
 constexpr int XSTEP = 3100 * 2 + 500 - STEP_BACK_X * 2;
 constexpr int YSTEP = 1500 * 2 + 100 - STEP_BACK_Y;
