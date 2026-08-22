448d636a58014eff772f493082974b5e40517a1b
motty <motty.mio2@gmail.com>
2026-08-22 19:04:53 +0900

    update cketch

--- sketch/motor_config.h
+++ sketch/motor_config.h
@@ -12,8 +12,8 @@ constexpr int resolution = 8;
 // 盤面の可動最大ステップ数
 constexpr int STEP_BACK = 100;     // ソフトウェアの可動範囲マージン
 constexpr int PHYSICAL_BACK = 300; // 物理的なリミットスイッチ離脱距離
-constexpr int XSTEP = 3100 * 2 - STEP_BACK * 2;
-constexpr int YSTEP = 1500 * 2 - STEP_BACK;
+constexpr int XSTEP = 3100 * 2 + 100 - STEP_BACK * 2;
+constexpr int YSTEP = 1500 * 2 + 50 - STEP_BACK;
 
 constexpr long HOMING_CHUNK = -10;
 
